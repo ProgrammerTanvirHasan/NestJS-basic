6d871be2ef3e82e2be6312b1516150d362551366
Md.Tanvir <hasanmdtanvir916@gmail.com>
2025-12-28 14:08:03 +0600

    added simple crud operato

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Put } from '@nestjs/common';
 import { UsersService } from './users.service';
 
 @Controller('users')
@@ -13,4 +13,25 @@ export class UsersController {
   getUserById(@Param('id') id: string) {
     return this.userService.getUserById(Number(id));
   }
+
+  @Post() //@Body decorator use kora hoyece karon je request ta ashbe sheita Body tei ashbe
+  createUser(@Body() body:{name:string,age:number}){
+    return this.userService.createPost(body)
+  }
+  
+  @Put(':id')
+  updateUser(@Param('id') id:string, @Body() body:{name:string,age:number}){
+  return this.userService.updateItem(Number(id),body)
+  }
+
+
+  @Patch(':id')
+   patchUser(@Param('id') id:string,@Body() body :Partial<{name:string,age:number}>){
+    return this.userService.patchItem(Number(id),body)
+   }
+
+   @Delete(':id')
+   deleteUser(@Param('id') id:string){
+    return this.userService.deletedItem(Number(id))
+   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,16 +1,50 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 
 @Injectable()
 export class UsersService {
   private items = [
-    { id: 1, name: 'john' },
-    { id: 2, name: 'pohn' },
-    { id: 3, name: 'lohn' },
+    { id: 1, name: 'john',age:32 },
+    { id: 2, name: 'pohn',age:40 },
+    { id: 3, name: 'lohn',age:26 },
   ];
   getAllUsers() {
     return this.items;
   }
   getUserById(id: number) {
-    return this.items.find((item) => item.id === id);
+    const item= this.items.find((item) => item.id === id);
+    if(!item) throw new NotFoundException("item not found");
+    return item;
   }
+   
+  createPost(data:{name:string;age:number}){
+     const newItem={
+      id:Date.now(),
+      ...data
+     }
+     this.items.push(newItem)
+     return newItem;
+  }
+
+  updateItem(id:number,data:{name:string,age:number}){
+    const itemIntex=this.items.findIndex((item)=>item.id===id)
+    if(itemIntex=== -1) throw new NotFoundException("this item not found to update");
+    this.items[itemIntex]={id,...data};
+    return this.items[itemIntex];
+  }
+
+  patchItem(id:number,data:Partial<{name:string,age:number}>){
+    const fixedItem=this.getUserById(id);
+    Object.assign(fixedItem,data);
+    return fixedItem;
+  }
+
+  deletedItem(id:number){
+ const index=this.items.findIndex((item)=>item.id===id)
+ if(index=== -1) throw new NotFoundException("no item found to delete");
+ const currentUser=this.items.splice(index,1);
+ return {message:"items deleted",item:currentUser[0]};
+
+  }
+  
+
 }
